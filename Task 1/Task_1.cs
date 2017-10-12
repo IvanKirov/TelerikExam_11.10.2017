a616efc52050e567cd0953b44cd8bc537221c10f
IvanKirov <ivankirov55@gmail.com>
2017-10-12 13:51:24 +0300

    Task 1 complete, not tested in http://bgcoder.com.

---
+++ Task 1/Task_1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task_1
+{
+    public class CrookedDigits
+    {
+        public static void Main()
+        {
+            var inputN = Console.ReadLine();
+            var answer = 0;
+
+            while (inputN.Length > 1)
+            {                
+                for (int i = 0; i < inputN.Length; i++)
+                {
+                    var newDigit = inputN[i];
+                    if (newDigit == '.' || newDigit == '-') { }
+                    else
+                    {
+                        answer += (newDigit - 48);
+                    }
+                }
+                inputN = answer.ToString();
+                answer = 0;
+            }
+
+            if (answer == 0)
+            {
+                answer = int.Parse(inputN);
+            }
+
+            Console.WriteLine(answer);
+        }
+    }
+}
