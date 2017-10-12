9586e3b89983c0f381857a2de757ce76f0ff5803
IvanKirov <ivankirov55@gmail.com>
2017-10-12 16:10:25 +0300

    Added the third task.

---
+++ Task 3/Task_3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Task_3
+{
+    public class Crooked_Walls
+    {
+        public static void Main()
+        {
+            var wallsHeights = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            long answer = 0;
+
+            for (int i = 1; i < wallsHeights.Length; i++)
+            {
+                var currentWallHeight = wallsHeights[i];
+                var previousWallHeight = wallsHeights[i - 1];
+                var highWall = Math.Max(currentWallHeight, previousWallHeight);
+                var lowWall = Math.Min(currentWallHeight, previousWallHeight);
+                var distance = highWall - lowWall;
+
+                if (distance % 2 == 0)
+                {
+                    answer += distance;
+                    i++;
+                }
+            }
+
+            Console.WriteLine(answer);
+        }
+    }
+}
